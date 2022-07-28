2549e537f68d5a41399b75d4c681200fa18d1089
JamaNegmatulin <jamanegmatulin@gmail.com>
2022-07-28 22:11:27 +0500

    Вытенул вопросы с базы данных

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
-  styleUrls: ['./quiz.component.scss']
+  styleUrls: ['./quiz.component.scss'],
 })
 export class QuizComponent implements OnInit {
+  public questions$: Observable<any[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private firestore: Firestore) {
+    const questions = collection(firestore, 'quizes/hKQPv0pxOUTg6SMtX46g/questions');
+    this.questions$ = collectionData(questions, { idField: 'uid' });
   }
 
+  ngOnInit(): void {}
 }
